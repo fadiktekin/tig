8d0ed8e809ea89a5d3579078b79a6a75e2e5a287
Fadik Tekin <tekinfadik@gmail.com>
2024-11-07 09:10:03 +0100

    fix import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import {
-  DocumentHeadTags,
-  documentGetInitialProps,
-} from "@mui/material-nextjs/v15-pagesRouter";
 
 export default function Document() {
   return (
