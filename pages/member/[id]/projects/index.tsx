989d3eee57fc5b59cca7f59be93b16e13020e8be
Fadik Tekin <tekinfadik@gmail.com>
2024-11-08 14:09:32 +0100

    reset change

--- pages/member/[id]/projects/index.tsx
+++ pages/member/[id]/projects/index.tsx
@@ -1,24 +1,3 @@
-import { useSession, signIn, signOut } from "next-auth/react";
-
 export default function Projects() {
-  const { data, status } = useSession();
-  if (status === "loading") return <h1> loading... please wait</h1>;
-  if (status === "authenticated") {
-    return (
-      <div>
-        <h1> hi {(data as any).user.name}</h1>
-        <img
-          src={(data as any).user.image}
-          alt={(data as any).user.name + " photo"}
-        />
-        <button onClick={() => signOut()}>sign out</button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <button onClick={() => signIn("google")}>sign in with gooogle</button>
-    </div>
-  );
+  return <>I'm members projects</>;
 }
