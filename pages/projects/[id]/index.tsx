23e00766dcd3b878cafd57f4ca8eada0531a07cc
Fadik Tekin <tekinfadik@gmail.com>
2024-11-19 20:13:50 +0100

    add details to projects

--- pages/projects/[id]/index.tsx
+++ pages/projects/[id]/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
-import { Button, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import { StatusTag } from "@/components/StatusTag";
+import Image from "next/image";
+import useSWR from "swr";
 import { capitalizeFirstLetter } from "@/components/utils/capitalizeFirstLetter";
 
 export default function ProjectDetails() {
@@ -16,14 +17,61 @@ export default function ProjectDetails() {
 
   if (!isReady || isLoading || error) return <>Loading...</>;
 
-  const { title, status } = data;
+  const {
+    images,
+    title,
+    description,
+    status,
+    startDate,
+    endDate,
+    expense,
+    price,
+  } = data;
 
   return (
-    <div className="flex justify-between p-4">
-      <div className="flex items-center gap-4">
-        <Typography variant="h4">{capitalizeFirstLetter(title)}</Typography>
-        <StatusTag status={status} />
+    <section className="flex justify-center p-4">
+      <div className="w-1/2">
+        <div className="flex items-center gap-2 mb-4">
+          <Typography variant="h4">{capitalizeFirstLetter(title)}</Typography>
+          <StatusTag status={status} />
+        </div>
+        <article className="flex flex-col gap-4">
+          {!!images.length && (
+            <Paper elevation={3} className="p-4">
+              <section className="flex gap-4 flex-wrap">
+                {images.map((imageUrl: string) => (
+                  <Image
+                    key={imageUrl}
+                    src={imageUrl}
+                    priority
+                    alt="project image"
+                    width={200}
+                    height={200}
+                  />
+                ))}
+              </section>
+            </Paper>
+          )}
+          <Paper elevation={3} className="p-4">
+            <Typography variant="subtitle1">
+              Start date: <b>{startDate}</b>
+            </Typography>
+            <Typography variant="subtitle1">
+              Finish date: <b>{endDate}</b>
+            </Typography>
+            <Typography variant="subtitle1">
+              Expense: <b>{expense}</b>
+            </Typography>
+            <Typography variant="subtitle1">
+              Price: <b>{price}</b>
+            </Typography>
+            <Typography variant="h5" className="mt-4">
+              Description
+            </Typography>
+            <Typography variant="body1">{description}</Typography>
+          </Paper>
+        </article>
       </div>
-    </div>
+    </section>
   );
 }
